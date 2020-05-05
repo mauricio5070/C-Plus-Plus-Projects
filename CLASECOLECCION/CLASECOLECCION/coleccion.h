cd273e3846782a6e518dac8a13d5e8a69b4e875a
mauricio5070 <46932866+mauricio5070@users.noreply.github.com>
2020-05-04 22:24:31 -0600

    Add files via upload

---
+++ CLASECOLECCION/CLASECOLECCION/coleccion.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>;
+#include <String>;
+using namespace std;
+
+
+const int dia = 6;
+const int hora = 10;
+
+
+class  Coleccion {
+private:
+	string vacaciones[fila][columna];
+	
+
+
+};
---
+++ CLASECOLECCION/CLASECOLECCION/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+using namespace std;
+
+
+int main()
+{
+	string horario [6][10] = { { "caminar",0,0,0,0,0,0,0,0,0 },
+	{ 0,"jugar",0,0,0,0,0,"fundamentos de informatica",0,0, },
+	{ 0,0,"cine","cine",0,0,0,0,0,0 },
+	{ 0,0,"natacion","natacion","natacion","natacion",0,"fundamentos de informatica",0,0 },
+	{ "lectura","lectura","lectura","lectura",0,0,0,0,0,0 } };
+	
+
+
+
+
+
+
+	system("PAUSE");
+	return 0;
+}
